4b80be5c36ee07a2fd04f61812f1c0b8772bbfd4
Hippolyte Joubin <hippolytejoubin@MacBook-Air-de-Hippolyte.local>
2025-03-04 13:54:30 +0100

    Base app setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TheaterSentimentDashboard from './TheaterSentimentDashboard';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <TheaterSentimentDashboard />
+    </div>
+  );
+}
+
+export default App;
